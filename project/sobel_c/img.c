07fb255e64c30e769d77828f16ab1701a1a7328c
Matthias Riegler <matthias.riegler@traefik.io>
2022-01-11 12:40:29 +0100

    feat: image transform grayscale -> color & back
    Signed-off-by: Matthias Riegler <matthias.riegler@traefik.io>

--- project/sobel_c/img.c
+++ project/sobel_c/img.c
@@ -29,6 +29,7 @@ ppm_image *load_image(const char *filename)
     }
 
     img = (ppm_image *)malloc(sizeof(ppm_image));
+    img->depth = 3;
     if (!img)
     {
         fprintf(stderr, "[!] Failed to alloc memory");
@@ -42,7 +43,7 @@ ppm_image *load_image(const char *filename)
         return NULL;
     }
 
-    //check if image is greyscale (only option supported)
+    //check if image is grayscale (only option supported)
     int max_pix_value;
     if (fscanf(fp, "%d", &max_pix_value) != 1)
     {
@@ -63,7 +64,7 @@ ppm_image *load_image(const char *filename)
         return NULL;
     }
 
-    //read pixel data from file, assume we're having a greyscale image -> only read one pixel value
+    //read pixel data from file, assume we're having a grayscale image -> only read one pixel value
     if (fread(img->data, 3 * img->size_x, img->size_y, fp) != img->size_y)
     {
         fprintf(stderr, "'%s' couldn't be loaded", filename);
@@ -79,6 +80,11 @@ ppm_image *load_image(const char *filename)
 int save_image(const char *filename, ppm_image *img)
 {
     FILE *fp;
+
+    if (img->depth != 3)
+    {
+        return -1;
+    }
 
     //open PPM file for reading
     fp = fopen(filename, "wb");
@@ -99,4 +105,62 @@ int save_image(const char *filename, ppm_image *img)
 
     fclose(fp);
     return 0;
+}
+
+// color_to_gray reduces the RGB color range to a single dimension.
+ppm_image *color_to_gray(ppm_image *in)
+{
+    ppm_image *out;
+
+    if (in->depth != 3)
+    {
+        return NULL;
+    }
+
+    // Allocate
+    out = (ppm_image *)malloc(sizeof(ppm_image));
+    out->data = (uint8_t *)malloc(in->size_x * in->size_y);
+    out->size_x = in->size_x;
+    out->size_y = in->size_y;
+    out->depth = 1;
+
+    for (uint64_t x = 0; x < in->size_x; x++)
+    {
+        for (uint64_t y = 0; y < in->size_y; y++)
+        {
+            out->data[y * out->size_x + x] = in->data[y * in->size_x * in->depth + x*3];
+        }
+    }
+    return out;
+}
+
+// greay_to_color explodes a single dimension to RGB
+ppm_image *gray_to_color(ppm_image *in)
+{
+    ppm_image *out;
+
+    if (in->depth != 1)
+    {
+        return NULL;
+    }
+
+    // Allocate
+    out = (ppm_image *)malloc(sizeof(ppm_image));
+    out->data = (uint8_t *)malloc(3 * in->size_x * in->size_y);
+    out->size_x = in->size_x;
+    out->size_y = in->size_y;
+    out->depth = 3;
+
+    for (uint64_t x = 0; x < in->size_x; x++)
+    {
+        for (uint64_t y = 0; y < in->size_y; y++)
+        {
+            for (uint8_t d = 0; d < out->depth; d++)
+            {
+                out->data[y * out->size_x * out->depth + x + d] = in->data[y * in->size_x * in->depth + x];
+            }
+        }
+    }
+
+    return out;
 }
