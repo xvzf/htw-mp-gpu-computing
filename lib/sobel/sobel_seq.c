56bd5de6e637f1f3f822a8af4e3a2b7b83b22551
Patrick Plewka <patrickplewka@googlemail.com>
2022-02-05 19:11:02 +0100

    Small optimization of sequential code

--- lib/sobel/sobel_seq.c
+++ lib/sobel/sobel_seq.c
@@ -7,13 +7,17 @@
 int sobel(ppm_image *in_img, ppm_image *out_img)
 {
     // FIXME add input validation
-    for (uintmax_t x = 0; x < in_img->size_x - 2; x++)
+
+    int max_x = in_img->size_x - 2;
+    int max_y = in_img->size_y - 2;
+    for (uintmax_t x = 0; x < max_x; x++)
     {
-        for (uintmax_t y = 0; y < in_img->size_y - 2; y++)
-        {
+        for (uintmax_t y = 0; y < max_y; y++) {
+
             out_img->data[y * out_img->size_x + x] = sobel_combined_op(in_img, x, y);
         }
     }
+
 
     return 0;
 }
