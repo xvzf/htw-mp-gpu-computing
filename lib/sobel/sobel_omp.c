e26b7060bd3fa6018eddc206bdb703620b476163
Matthias Riegler <matthias@xvzf.tech>
2022-02-05 10:53:28 +0100

    fix: OMP implentation uses predictable chunks
    Note: this only works with a fixed number of CPU cores (currently 32)

--- lib/sobel/sobel_omp.c
+++ lib/sobel/sobel_omp.c
@@ -1,20 +1,38 @@
+#include <stdio.h>
 #include <stdint.h>
 #include <omp.h>
 
 #include "lib/img/types.h"
 #include "sobel.h"
 
+#define OMP_SPLIT_CHUNKS 32 // Not ideal as there's no auto-detection of CPU cores but required for OMP
+
 // Sequential sobel implementation
 int sobel(ppm_image *in_img, ppm_image *out_img)
 {
+    const uintmax_t chunk_size = ((in_img->size_x - 2) / OMP_SPLIT_CHUNKS);
+    const uintmax_t chunk_loss = (in_img->size_x - 2) % OMP_SPLIT_CHUNKS;
+    printf("Chunk size: %ld, Chunk loss: %ld\n", chunk_size, chunk_loss);
+
 // FIXME add input validation
-#pragma omp parallel for
-    for (uintmax_t x = 0; x < in_img->size_x - 2; x++)
+#pragma omp parallel for schedule(dynamic, 1)
+    for (int i = 0; i < OMP_SPLIT_CHUNKS; i++)
     {
-        // Not neccessary to further accelerate it as the workload is already evenly distributed with the
-        for (uintmax_t y = 0; y < in_img->size_y - 2; y++)
+        // Have a deterministic for loop chunking our workload.
+
+        uintmax_t x = chunk_size * (uintmax_t)i + (i == 0 ? 0 : chunk_loss);
+        uintmax_t x_to = chunk_size * ((uintmax_t)i + 1) + chunk_loss;
+
+        printf("%ld -> %ld\n", x, x_to);
+
+        // Actual computation
+        for (; x < x_to; x++)
         {
-            out_img->data[y * out_img->size_x + x] = sobel_combined_op(in_img, x, y);
+            // Not neccessary to further accelerate it as the workload is already evenly distributed with the
+            for (uintmax_t y = 0; y < in_img->size_y - 2; y++)
+            {
+                out_img->data[y * out_img->size_x + x] = sobel_combined_op(in_img, x, y);
+            }
         }
     }
 
